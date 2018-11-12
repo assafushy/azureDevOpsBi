a0889387726982e740f7c679e77d40c4135eb132
assafushy Dell <assafushy@gmail.com>
2018-11-12 21:50:52 +0200

    added matarial ui created drawer component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
 import React, { Component } from 'react';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      
+      "toggleDrawerMenu":true
+    }
+  }//constructor
+
+  toggleDrawer(){
+    (this.state.toggleDrawerMenu)?
+    this.setState({"toggleDrawerMenu":false}):
+    this.setState({"toggleDrawerMenu":true});
+  }//toggleDrawer
+
+
   render() {
     return (
       <div className="App">
-       
+       <Drawer anchor="left" open={this.state.toggleDrawerMenu} onClose={()=>{this.toggleDrawer()}}>
+          <div
+            tabIndex={0}
+            role="button"
+            onClick={()=>{this.toggleDrawer()}}
+            onKeyDown={()=>{this.toggleDrawer()}}
+          >
+           <List>
+            <ListItem button key={'code'}>
+              <ListItemIcon></ListItemIcon>
+              <ListItemText primary={'Code'} />
+            </ListItem>
+          </List> 
+          </div>
+        </Drawer>
       </div>
     );
   }
