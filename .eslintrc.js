7dac52e23aae44947e892592fcf67d0c4efbdf27
Nadav Cohen <EI50019@esl.corp.elbit.co.il>
2018-12-12 21:12:56 +0200

    add list buttons to drawer, add eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+	plugins: ["react"],
+	rules: {
+		indent: ["error", "tab"],
+		"linebreak-style": ["error", "windows"],
+		quotes: ["error", "double"],
+		semi: ["error", "always"]
+	}
+};
--- src/App.js
+++ src/App.js
@@ -1,93 +1,134 @@
-import React, { Component } from 'react';
-import {bindActionCreators} from 'redux';
-import {connect,} from 'react-redux';
-import {fetchAllServerProjects} from './redux/actions/globalDataActions';
-import {setSelectedProjects} from '../src/redux/actions/globalDataActions';
+import React, { Component } from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import { fetchAllServerProjects } from "./redux/actions/globalDataActions";
+import { setSelectedProjects } from "../src/redux/actions/globalDataActions";
 
-import MainBar from './components/appBar/MainBar';
-import Drawer from '@material-ui/core/Drawer';
-import CodeDashboard from './components/codePage/CodeDashboard';
+import MainBar from "./components/appBar/MainBar";
+import Drawer from "@material-ui/core/Drawer";
+import CodeDashboard from "./components/codePage/CodeDashboard";
 
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import CodeIcon from '@material-ui/icons/Code';
-import Button from '@material-ui/core/Button';
-import CachedIcon from '@material-ui/icons/Cached';
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import CodeIcon from "@material-ui/icons/Code";
+import Button from "@material-ui/core/Button";
+import CachedIcon from "@material-ui/icons/Cached";
+import Divider from "@material-ui/core/Divider";
 
 const fabStyle = {
-  bottom: 20,
-  right: 10,
-  position: 'fixed'
+	bottom: 20,
+	right: 10,
+	position: "fixed"
 };
 
 class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			toggleDrawerMenu: false
+		};
+	} //constructor
 
-  constructor(){
-    super();
-    this.state = {   
-      "toggleDrawerMenu":false
-    }
-  }//constructor
+	componentDidMount() {
+		this.props.fetchAllServerProjects();
+	} //componentDidMount
 
-  componentDidMount(){
-    this.props.fetchAllServerProjects();
-  }//componentDidMount
+	toggleDrawer() {
+		this.state.toggleDrawerMenu
+			? this.setState({ toggleDrawerMenu: false })
+			: this.setState({ toggleDrawerMenu: true });
+	} //toggleDrawer
 
-  toggleDrawer(){
-    (this.state.toggleDrawerMenu)?
-    this.setState({"toggleDrawerMenu":false}):
-    this.setState({"toggleDrawerMenu":true});
-  }//toggleDrawer
+	render() {
+		const { classes, theme } = this.props;
 
-  render() {
-    return (
-      <div className="App">
-      <MainBar toggleMenu={this.toggleDrawer.bind(this)} globalData={this.props.globalData}/>
-       <Drawer anchor="left" open={this.state.toggleDrawerMenu} onClose={()=>{this.toggleDrawer()}}>
-          <div
-            tabIndex={0}
-            role="button"
-            onClick={()=>{this.toggleDrawer()}}
-            onKeyDown={()=>{this.toggleDrawer()}}
-          >
-           <List>
-            <ListItem button key={'code'}>
-              <ListItemIcon><CodeIcon/></ListItemIcon>
-              <ListItemText primary={'Code'} />
-            </ListItem>
-          </List> 
-          </div>
-        </Drawer>
-        <CodeDashboard globalData={this.props.globalData} codeData={this.props.codeData}/>
-        <Button 
-          style={fabStyle}
-          onClick={()=>{setSelectedProjects()}}  
-          variant="fab" 
-          color="primary" 
-          aria-label="Add">
-          <CachedIcon/>
-        </Button>
-        
-      </div>
-    );
-  }//render
+		const drawer = (
+			<div>
+				<div />
+				<Divider />
+				<List>
+					{["Code", "Build", "Test", "Release"].map((text, index) => (
+						<ListItem button key={text}>
+							<ListItemText primary={text} />
+						</ListItem>
+					))}
+				</List>
+				<Divider />
+				<List>
+					{["Settings"].map((text, index) => (
+						<ListItem button key={text}>
+							<ListItemText primary={text} />
+						</ListItem>
+					))}
+				</List>
+			</div>
+		);
 
-}//class
+		return (
+			<div className="App">
+				<MainBar
+					toggleMenu={this.toggleDrawer.bind(this)}
+					globalData={this.props.globalData}
+				/>
+				<Drawer
+					anchor="left"
+					open={this.state.toggleDrawerMenu}
+					onClose={() => {
+						this.toggleDrawer();
+					}}
+				>
+					<div
+						tabIndex={0}
+						role="button"
+						onClick={() => {
+							this.toggleDrawer();
+						}}
+						onKeyDown={() => {
+							this.toggleDrawer();
+						}}
+					>
+						{drawer}
+					</div>
+				</Drawer>
+				<CodeDashboard
+					globalData={this.props.globalData}
+					codeData={this.props.codeData}
+				/>
+				<Button
+					style={fabStyle}
+					onClick={() => {
+						setSelectedProjects();
+					}}
+					variant="fab"
+					color="primary"
+					aria-label="Add"
+				>
+					<CachedIcon />
+				</Button>
+			</div>
+		);
+	} //render
+} //class
 
-function mapStateToProps(state){
-  return{
-    globalData:state.globalData,
-    codeData:state.codeData
-  }
-}//mapStateToProps
+function mapStateToProps(state) {
+	return {
+		globalData: state.globalData,
+		codeData: state.codeData
+	};
+} //mapStateToProps
 
-function matchDispachToProps(dispatch){
-  return bindActionCreators({
-    fetchAllServerProjects
-  }
-  ,dispatch)
-}//matchDispachToProps
+function matchDispachToProps(dispatch) {
+	return bindActionCreators(
+		{
+			fetchAllServerProjects
+		},
+		dispatch
+	);
+} //matchDispachToProps
 
-export default connect(mapStateToProps,matchDispachToProps)(App);
+export default connect(
+	mapStateToProps,
+	matchDispachToProps
+)(App);
