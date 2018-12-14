2d6a70f1f37e82ba60bb32563ee8cfae897d1962
Nadav Cohen <EI50019@esl.corp.elbit.co.il>
2018-12-14 09:16:20 +0200

    eslint fixes

--- src/App.js
+++ src/App.js
@@ -18,117 +18,117 @@ import CachedIcon from "@material-ui/icons/Cached";
 import Divider from "@material-ui/core/Divider";
 
 const fabStyle = {
-	bottom: 20,
-	right: 10,
-	position: "fixed"
+  bottom: 20,
+  right: 10,
+  position: "fixed"
 };
 
 class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			toggleDrawerMenu: false
-		};
-	} //constructor
+  constructor() {
+    super();
+    this.state = {
+      toggleDrawerMenu: false
+    };
+  } //constructor
 
-	componentDidMount() {
-		this.props.fetchAllServerProjects();
-	} //componentDidMount
+  componentDidMount() {
+    this.props.fetchAllServerProjects();
+  } //componentDidMount
 
-	toggleDrawer() {
-		this.state.toggleDrawerMenu
-			? this.setState({ toggleDrawerMenu: false })
-			: this.setState({ toggleDrawerMenu: true });
-	} //toggleDrawer
+  toggleDrawer() {
+    this.state.toggleDrawerMenu
+      ? this.setState({ toggleDrawerMenu: false })
+      : this.setState({ toggleDrawerMenu: true });
+  } //toggleDrawer
 
-	render() {
-		const { classes, theme } = this.props;
+  render() {
+    const { classes, theme } = this.props;
 
-		const drawer = (
-			<div>
-				<div />
-				<Divider />
-				<List>
-					{["Code", "Build", "Test", "Release"].map((text, index) => (
-						<ListItem button key={text}>
-							<ListItemText primary={text} />
-						</ListItem>
-					))}
-				</List>
-				<Divider />
-				<List>
-					{["Settings"].map((text, index) => (
-						<ListItem button key={text}>
-							<ListItemText primary={text} />
-						</ListItem>
-					))}
-				</List>
-			</div>
-		);
+    const drawer = (
+      <div>
+        <div />
+        <Divider />
+        <List>
+          {["Code", "Build", "Test", "Release"].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {["Settings"].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </div>
+    );
 
-		return (
-			<div className="App">
-				<MainBar
-					toggleMenu={this.toggleDrawer.bind(this)}
-					globalData={this.props.globalData}
-				/>
-				<Drawer
-					anchor="left"
-					open={this.state.toggleDrawerMenu}
-					onClose={() => {
-						this.toggleDrawer();
-					}}
-				>
-					<div
-						tabIndex={0}
-						role="button"
-						onClick={() => {
-							this.toggleDrawer();
-						}}
-						onKeyDown={() => {
-							this.toggleDrawer();
-						}}
-					>
-						{drawer}
-					</div>
-				</Drawer>
-				<CodeDashboard
-					globalData={this.props.globalData}
-					codeData={this.props.codeData}
-				/>
-				<Button
-					style={fabStyle}
-					onClick={() => {
-						setSelectedProjects();
-					}}
-					variant="fab"
-					color="primary"
-					aria-label="Add"
-				>
-					<CachedIcon />
-				</Button>
-			</div>
-		);
-	} //render
+    return (
+      <div className="App">
+        <MainBar
+          toggleMenu={this.toggleDrawer.bind(this)}
+          globalData={this.props.globalData}
+        />
+        <Drawer
+          anchor="left"
+          open={this.state.toggleDrawerMenu}
+          onClose={() => {
+            this.toggleDrawer();
+          }}
+        >
+          <div
+            tabIndex={0}
+            role="button"
+            onClick={() => {
+              this.toggleDrawer();
+            }}
+            onKeyDown={() => {
+              this.toggleDrawer();
+            }}
+          >
+            {drawer}
+          </div>
+        </Drawer>
+        <CodeDashboard
+          globalData={this.props.globalData}
+          codeData={this.props.codeData}
+        />
+        <Button
+          style={fabStyle}
+          onClick={() => {
+            setSelectedProjects();
+          }}
+          variant="fab"
+          color="primary"
+          aria-label="Add"
+        >
+          <CachedIcon />
+        </Button>
+      </div>
+    );
+  } //render
 } //class
 
 function mapStateToProps(state) {
-	return {
-		globalData: state.globalData,
-		codeData: state.codeData
-	};
+  return {
+    globalData: state.globalData,
+    codeData: state.codeData
+  };
 } //mapStateToProps
 
 function matchDispachToProps(dispatch) {
-	return bindActionCreators(
-		{
-			fetchAllServerProjects
-		},
-		dispatch
-	);
+  return bindActionCreators(
+    {
+      fetchAllServerProjects
+    },
+    dispatch
+  );
 } //matchDispachToProps
 
 export default connect(
-	mapStateToProps,
-	matchDispachToProps
+  mapStateToProps,
+  matchDispachToProps
 )(App);
