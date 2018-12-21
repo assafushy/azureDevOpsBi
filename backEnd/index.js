b1b0b6889bf8c7a78f90fcd16b99d332accaff2c
assafushy Dell <assafushy@gmail.com>
2018-12-21 14:20:51 +0200

    changing from graphql to REST

--- backEnd/index.js
+++ backEnd/index.js
@@ -1,21 +1,12 @@
 var express = require('express');
-var express_graphql = require('express-graphql');
-var { buildSchema } = require('graphql');
-// GraphQL schema
-var schema = buildSchema(`
-    type Query {
-        message: String
-    }
-`);
-// Root resolver
-var root = {
-    message: () => 'Hello World!'
-};
-// Create an express server and a GraphQL endpoint
-var app = express();
-app.use('/graphql', express_graphql({
-    schema: schema,
-    rootValue: root,
-    graphiql: true
-}));
-app.listen(4000, () => console.log('Express GraphQL Server Now Running On localhost:4000/graphql'));
+const app = express();
+const logger = require('morgan');
+const cors = require('cors');
+
+app.use(cors());
+// if(config.env !== "test"){app.use(logger('dev'));}
+
+// const viewsRouter = require('./api/views');
+// app.use('/views',viewsRouter);
+
+app.listen(4000, () => console.log('apo running on http://localhost:4000'));
