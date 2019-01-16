f2e7ffe1707d0854f6fac570c5290b0967f2658b
assafushy Dell <assafushy@gmail.com>
2019-01-16 09:57:38 +0200

    added top filter to projects get request

--- frontEnd/src/azureDevopsRESTAPI/projectData.js
+++ frontEnd/src/azureDevopsRESTAPI/projectData.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 //fetches all teamProjects
 export async function getProjectList(collection) { 
+  let top = 1024;
   //https://assafushy.visualstudio.com/defaultcollection/_apis/projects?api-version=1.0
-    return axios.get(`${Config.BASE_URL}/_apis/projects?api-version=1.0`);
+    return axios.get(`${Config.BASE_URL}/_apis/projects?$top=${top}&api-version=4.0`);
 }//getProjectList
