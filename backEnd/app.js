ac5a0b4cdbb42b0afc49ae7a296e0a8f4adad879
assafushy Dell <assafushy@gmail.com>
2019-03-23 22:43:59 +0200

    used dotenv for env vars

--- backEnd/app.js
+++ backEnd/app.js
@@ -1,11 +1,13 @@
-'use strict';
+"use strict";
 
-var SwaggerExpress = require('swagger-express-mw');
-var app = require('express')();
-const mongoose = require('mongoose');
-const bluebird = require('bluebird');
-const cors = require('cors');
-const morgan = require('morgan');
+var SwaggerExpress = require("swagger-express-mw");
+var app = require("express")();
+const mongoose = require("mongoose");
+const bluebird = require("bluebird");
+const cors = require("cors");
+const morgan = require("morgan");
+
+require("dotenv").config();
 
 module.exports = app; // for testing
 
@@ -14,23 +16,31 @@ var config = {
 };
 
 SwaggerExpress.create(config, function(err, swaggerExpress) {
-  if (err) { throw err; }
+  if (err) {
+    throw err;
+  }
   app.use(cors());
   app.use(morgan("tiny"));
   // install middleware
   swaggerExpress.register(app);
 
   var port = process.env.PORT || 10010;
-  
+
   mongoose.Promise = bluebird;
-  console.log(`container is up with ENV var DOCKER = ${JSON.stringify(process.env.DOCKER)}`);
-  let mongoUrl = (process.env.DOCKER)? 'mongodb://mongo:27017/azuredevopsbi' : 'mongodb://localhost:27017/azuredevopsbi';
-  mongoose.connect(mongoUrl,{ useNewUrlParser: true });
-  mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
-  mongoose.connection.once('open',()=>{
-    console.log(`server is listening on ${port}`);  
+  console.log(
+    `container is up with ENV var DOCKER = ${JSON.stringify(
+      process.env.DOCKER
+    )}`
+  );
+  let mongoUrl = process.env.mongoUrl;
+  mongoose.connect(mongoUrl, { useNewUrlParser: true });
+  mongoose.connection.on(
+    "error",
+    console.error.bind(console, "connection error:")
+  );
+  mongoose.connection.once("open", () => {
+    console.log(`server is listening on ${port}`);
     console.log("API server is connected to DB!");
     app.listen(port);
   });
-
 });
