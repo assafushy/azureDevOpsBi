b145c82e03d103eec09e4d70dfed6577a463c050
assafushy <assafushy@gmail.com>
2019-07-22 22:36:57 +0300

    refactored fetch build data function to sum the data correctly

--- frontEnd/src/configFiles/config.js
+++ frontEnd/src/configFiles/config.js
@@ -1,17 +1,18 @@
 //default config object
 var config = {
-    dev:'development',
-    BI_API_BASE_URL:'',
-    port: process.env.PORT || 3000,
-    BASE_URL:process.env.REACT_APP_URL || "https://assafushy.visualstudio.com",
-    AccessToken:process.env.REACT_APP_APIKEY || "7ne3n2b4uzgux46bkjvofdz7lsz2kbb4nm34ai45jiztdichxeta"
-}
+  dev: "development",
+  BI_API_BASE_URL: "",
+  port: process.env.PORT || 3000,
+  BASE_URL: process.env.REACT_APP_URL,
+  AccessToken: process.env.REACT_APP_APIKEY
+};
 
-config.BI_API_BASE_URL =  (process.env.DOCKER)? 'http://backend:10010': 'http://localhost:10010';
+config.BI_API_BASE_URL = process.env.DOCKER
+  ? "http://backend:10010"
+  : "http://localhost:10010";
 
 console.log(`process.env.BASE_URL: ${process.env.REACT_APP_URL}`);
 console.log(`process.env.AccessToken: ${process.env.REACT_APP_APIKEY}`);
 console.log(`BI-API base url: ${config.BI_API_BASE_URL}`);
 
-
-module.exports =config;
+module.exports = config;
